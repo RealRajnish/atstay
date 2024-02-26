d3801104df8765c70dc1299027f1547e5950cffd
Snehal <snehalgadge205@gmail.com>
2024-02-26 17:23:32 +0530

    host is ready

---
+++ client/src/components/Listings.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { categories } from "../data";
+import "../styles/Listings.scss";
+import ListingCard from "./ListingCard";
+import Loader from "./Loader";
+import { useDispatch, useSelector } from "react-redux";
+import { setListings } from "../redux/state";
+import { productData } from "../temp/productData";
+
+
+
+const Listings = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const listings = useSelector((state) => state.listings);
+
+  const getFeedListings = async () => {
+    try {
+      const data = productData
+      dispatch(setListings({ listings: data }));
+      setLoading(false);
+    } catch (err) {
+      console.log("Fetch Listings Failed", err.message);
+    }
+  };
+
+  useEffect(() => {
+    getFeedListings();
+  }, [selectedCategory]);
+
+  return (
+    <>
+      <div className="category-list">
+        {categories?.map((category, index) => (
+          <div
+            className={`category ${category.label === selectedCategory ? "selected" : ""}`}
+            key={index}
+            onClick={() => setSelectedCategory(category.label)}
+          >
+            <div className="category_icon">{category.icon}</div>
+            <p>{category.label}</p>
+          </div>
+        ))}
+      </div>
+
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="listings">
+          {listings.map(
+            ({
+              _id,
+              photo,
+              place,
+              trip,
+              overview,
+              price,
+              booking=false
+            }) => (
+              <ListingCard
+                listingId={_id}
+                photo={photo}
+                city={place}
+                category={trip}
+                type={overview}
+                price={price}
+                booking={booking}
+              />
+            )
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Listings;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,135 @@
+import { IconButton } from "@mui/material";
+import { Search, Person, Menu } from "@mui/icons-material";
+import variables from "../styles/variables.scss";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "../styles/Navbar.scss";
+import { Link, useNavigate } from "react-router-dom";
+import { setLogout } from "../redux/state";
+import UpperNavbar from "./UpperNavbar";
+import LowerNavbar from "./LowerNavbar";
+
+const Navbar = () => {
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  const [search, setSearch] = useState("");
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <UpperNavbar />
+      <div className="navbar" style={{ gap: "3rem" }}>
+        <a href="/">
+          <img src="/assets/logo.webp" alt="logo" />
+        </a>
+        <div className="links-container">
+          <ul className="navbar-nav falja">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/attours" className="nav-link">
+                Tour
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/atstays" className="nav-link">
+                Stays
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/bloggerpage" className="nav-link">
+                Blogs
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar_search">
+          <input
+            type="text"
+            placeholder="Destination..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <IconButton disabled={search === ""}>
+            <Search
+              sx={{ color: "#66cccc" }}
+              onClick={() => {
+                navigate(`/properties/search/${search}`);
+              }}
+            />
+          </IconButton>
+        </div>
+
+        <div className="navbar_right">
+          {user ? (
+            <a href="/create-listing" className="host">
+              Become A Host
+            </a>
+          ) : (
+            <a href="/login" className="host">
+              Become A Host
+            </a>
+          )}
+
+          <button
+            className="navbar_right_account"
+            onClick={() => setDropdownMenu(!dropdownMenu)}
+          >
+            <Menu sx={{ color: variables.darkgrey }} />
+            {!user ? (
+              <Person sx={{ color: variables.darkgrey }} />
+            ) : (
+              <img
+                src={`http://localhost:3001/${user.profileImagePath.replace(
+                  "public",
+                  ""
+                )}`}
+                alt="profile photo"
+                style={{ objectFit: "cover", borderRadius: "50%" }}
+              />
+            )}
+          </button>
+
+          {dropdownMenu && !user && (
+            <div className="navbar_right_accountmenu">
+              <Link to="/login">Log In</Link>
+              <Link to="/register">Sign Up</Link>
+            </div>
+          )}
+
+          {dropdownMenu && user && (
+            <div className="navbar_right_accountmenu">
+              <Link to={`/${user._id}/trips`}>Trip List</Link>
+              <Link to={`/${user._id}/wishList`}>Wish List</Link>
+              <Link to={`/${user._id}/properties`}>Property List</Link>
+              <Link to={`/${user._id}/reservations`}>Reservation List</Link>
+              <Link to="/create-listing">Become A Host</Link>
+
+              <Link
+                to="/login"
+                onClick={() => {
+                  dispatch(setLogout());
+                }}
+              >
+                Log Out
+              </Link>
+            </div>
+          )}
+
+          </div>
+          </div>
+          <LowerNavbar />
+    </>
+  );
+};
+
+export default Navbar;
